931e7789633fd25937db55d197b49d02ddd79515
meepymeep <antonleonthompson@gmail.com>
2022-11-13 13:48:41 -0400

    Prints all the names in an array.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String[] names = {"Bob", "Joe", "Mama"};
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
